8fec8f0b1cb4a2b0dedd4c857e41066ac83cf090
Arx <Arximiro@outlook.com>
2017-10-01 18:39:46 -0400

    Impemented Index View and Action for Admin Posts.

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,7 @@
-﻿using SimpleBlog.Infrastructure;
+﻿using NHibernate.Linq;
+using SimpleBlog.Areas.Admin.ViewModels;
+using SimpleBlog.Infrastructure;
+using SimpleBlog.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +14,24 @@ namespace SimpleBlog.Areas.Admin.Controllers
     [SelectedTab("posts")]
     public class PostsController : Controller
     {
+        private const int postsPerPage = 5;
+
         // GET: Admin/Posts
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            var totalPostsCount = Database.Session.Query<Post>().Count();
+
+            var currentPostPage = Database.Session.Query<Post>()
+                .OrderByDescending(c => c.CreatedAt)
+                .Skip((page - 1) * postsPerPage)
+                .Take(postsPerPage)
+                .ToList();
+                
+
+            return View(new PostsIndex
+            {
+                Posts = new PagedData<Post>(currentPostPage, totalPostsCount, page, postsPerPage)
+            });
         }
     }
 }
