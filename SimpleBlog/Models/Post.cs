f42c0a1fe978a82a383ed3992043d2baa5f79658
Arx <Arximiro@outlook.com>
2017-10-03 02:49:59 -0400

    Added actions for Delete, Trash, and Restore. Implemented auto slugify component.

--- SimpleBlog/Models/Post.cs
+++ SimpleBlog/Models/Post.cs
@@ -50,7 +50,7 @@ namespace SimpleBlog.Models
             });
 
             Property(x => x.UpdatedAt, x => x.Column("updated_at"));
-            Property(x => x.UpdatedAt, x => x.Column("deleted_at"));
+            Property(x => x.DeletedAt, x => x.Column("deleted_at"));
 
             Bag(x => x.Tags, x =>
             {
